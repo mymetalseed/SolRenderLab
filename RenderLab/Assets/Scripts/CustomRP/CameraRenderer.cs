03889897552743b4d7a2ad4dd3cb14f16d5c81e3
834930269 <834930269@qq.com>
2023-06-04 23:13:23 +0800

    透明渲染 OK

--- RenderLab/Assets/Scripts/CustomRP/CameraRenderer.cs
+++ RenderLab/Assets/Scripts/CustomRP/CameraRenderer.cs
@@ -3,33 +3,87 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class CameraRenderer
+public partial class CameraRenderer
 {
     ScriptableRenderContext context;
     Camera camera;
+    CullingResults cullingResults;
+
+    const string bufferName = "Render Camera";
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
 
     public void Render(ScriptableRenderContext context,Camera camera)
     {
         this.context = context;
         this.camera = camera;
+        PrepareBuffer();
+        PrepareForSceneWindow();
+        //是否按照相机裁剪的?
+        if (!Cull())
+        {
+            return;
+        }
 
         Setup();
         DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
         Submit();
-    }
-
-    void DrawVisibleGeometry()
-    {
-        context.DrawSkybox(camera);
     }
 
     void Submit()
     {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
         context.Submit();
     }
 
     void Setup()
     {
         context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(flags<=CameraClearFlags.Depth, flags == CameraClearFlags.Color, flags == CameraClearFlags.Color?camera.backgroundColor.linear:Color.clear);
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
     }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        if(camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+    void DrawVisibleGeometry()
+    {
+        var sortingSettings = new SortingSettings(camera) { 
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawingSettings = new DrawingSettings(
+                unlitShaderTagId,sortingSettings
+            );
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+        context.DrawSkybox(camera);
+
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+        drawingSettings.sortingSettings = sortingSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
 }
